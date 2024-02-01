8d8053262611e97d36aa4dac578c410c473994c0
Andre Chen <101299705+andrechen77@users.noreply.github.com>
2024-02-01 15:38:19 -0600

    finish part 4

--- streamer.py
+++ streamer.py
@@ -6,20 +6,41 @@ import struct
 from concurrent.futures import ThreadPoolExecutor
 import time
 import threading
+import hashlib
 
 ack_timeout = 0.25
+hash_size = 4 # number of bytes
 packet_size = 1472
-packet_header_format = "!Ic"
-packet_header_size = struct.calcsize(packet_header_format)
+packet_header_format_wo_hash = f"!Ic" # 3-byte md5 hash
+packet_header_size = hash_size + struct.calcsize(packet_header_format_wo_hash)
 payload_size = packet_size - packet_header_size
 assert(payload_size > 0)
 
 # big-endian byte-ordering
 # 32-bit sequence number, packet type ('D', 'A', or 'F')
 
+def create_packet(seq_num: int, packet_type: bytes, payload: bytes) -> bytes:
+    packet_header_wo_hash = struct.pack(packet_header_format_wo_hash, seq_num, packet_type)
+    packet_wo_hash = packet_header_wo_hash + payload
+    hash = shake(packet_wo_hash, hash_size)
+    packet = hash + packet_wo_hash
+    # print(f"creating packet with hash {packet}")
+    return packet
+
+def dissect_packet(packet: bytes):
+    """Given a packet, returns a pair consisting of the headers tuple and the
+    payload or None if the hash doesn't match."""
+    expected_hash = packet[:hash_size]
+    packet_wo_hash = packet[hash_size:]
+    actual_hash = shake(packet_wo_hash, hash_size)
+    if expected_hash != actual_hash:
+        return None
+    seq_num, packet_type = struct.unpack_from(packet_header_format_wo_hash, packet_wo_hash)
+    payload = packet[packet_header_size:]
+    return (seq_num, packet_type), payload
+
 class Streamer:
-    def __init__(self, dst_ip, dst_port,
-                 src_ip=INADDR_ANY, src_port=0):
+    def __init__(self, dst_ip, dst_port, src_ip=INADDR_ANY, src_port=0):
         """Default values listen on all network interfaces, chooses a random source port,
            and does not introduce any simulated packet loss."""
         self.socket = LossyUDP()
@@ -28,7 +49,7 @@ class Streamer:
         self.dst_port = dst_port
         self.recv_buff_size = 128
         self.recv_buff = [None for _ in range(self.recv_buff_size)]
-        self.recv_seq_num = 0
+        self.recv_seq_num = 0 # the sequence number of the next packet expected by the receiver
         self.send_seq_num = 0 # the sequence number of the next packet to be sent
         self.send_ack_num = 0 # the sequence number of next packet that is awaiting acknowledgement
         self.send_got_ack = threading.Event()
@@ -38,21 +59,13 @@ class Streamer:
         executor = ThreadPoolExecutor(max_workers=1)
         executor.submit(self.listener)
 
-    def packetize(self, data_bytes):
+    def packetize(self, data_bytes: bytes):
         """Given a byte-array, iteratively returns the array transformed into packets."""
 
         for start in range(0, len(data_bytes), payload_size):
             payload = data_bytes[start:(start+payload_size)]
-            packet_header = struct.pack(packet_header_format, self.send_seq_num, b'D')
+            yield create_packet(self.send_seq_num, b'D', payload)
             self.send_seq_num += 1
-            yield packet_header + payload
-
-    def unpacketize(self, packet):
-        """Given a packet, returns a pair consisting of the headers tuple and the payload."""
-
-        seq_num, packet_type = struct.unpack_from(packet_header_format, packet)
-        payload = packet[packet_header_size:]
-        return (seq_num, packet_type), payload
 
     def send_packet(self, packet: bytes) -> None:
         """blocks until the packet is ACK'ed"""
@@ -100,25 +113,28 @@ class Streamer:
                     # the socket died
                     break
                 assert(len(packet) <= packet_size) # pretty sure self.socket.recvfrom() returns only one packet
-                headers, data = self.unpacketize(packet)
-                seq_num, packet_type = headers
+                maybe_dissected_packet = dissect_packet(packet)
+                if maybe_dissected_packet is None:
+                    print("this data sucks")
+                    continue
+                (seq_num, packet_type), data = maybe_dissected_packet
                 if packet_type == b'D':
                     if seq_num < self.recv_seq_num + self.recv_buff_size:
                         if self.recv_seq_num <= seq_num:
                             # the received packet can fit within the next recv_buff_size packets
                             self.recv_buff[seq_num % self.recv_buff_size] = data
                         # acknowledge packet even if we already received it (maybe our ACK got lost)
-                        ack = struct.pack(packet_header_format, seq_num, b'A') # no payload
+                        ack = create_packet(seq_num + 1, b'A', b'')
                         self.socket.sendto(ack, (self.dst_ip, self.dst_port))
                     # if the received packet can't fit or was already transmitted to the application, then drop it
                 elif packet_type == b'A':
-                    if seq_num >= self.send_ack_num:
+                    if seq_num > self.send_ack_num:
                         self.send_got_ack.set()
-                        self.send_ack_num = seq_num + 1
+                        self.send_ack_num = seq_num
                 elif packet_type == b'F':
                     self.got_fin.set()
                     # acknowledge packet even if we already received it (maybe our ACK got lost)
-                    ack = struct.pack(packet_header_format, seq_num, b'A') # no payload
+                    ack = create_packet(seq_num + 1, b'A', b'')
                     self.socket.sendto(ack, (self.dst_ip, self.dst_port))
                     print("got fin")
                 else:
@@ -133,11 +149,11 @@ class Streamer:
            the necessary ACKs and retransmissions"""
         # TODO at part 5 [Wait for any sent data packets to be ACKed. Actually, if you're doing stop-and-wait then you know all of your sent data has been ACKed. However, in Part 5 you'll add code to maybe wait here.]
         # Send a FIN packet.
-        fin = struct.pack(packet_header_format, self.send_seq_num, b'F') # no payload
+        fin = create_packet(self.send_seq_num, b'F', b'')
         # Wait for an ACK of the FIN packet. Go back to Step 2 if a timer expires.
-        print("starting to send fin")
+        # print("starting to send fin")
         self.send_packet(fin) # blocks until ACK'ed
-        print("finished fin")
+        # print("finished fin")
         # Wait until the listener records that a FIN packet was received from the other side.
         self.got_fin.wait() # blocks forever until a FIN comes TODO can we not???
         # Wait two seconds.
@@ -147,3 +163,8 @@ class Streamer:
         self.socket.stoprecv()
         # Finally, return from Streamer#close
         return
+
+def shake(data: bytes, length=hash_size):
+    m = hashlib.shake_128()
+    m.update(data)
+    return m.digest(length)
